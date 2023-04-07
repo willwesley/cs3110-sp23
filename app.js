8ba4c6cf476fd31beea6c2a1e37dc5ab638e1faa
Debian <admin@ip-172-31-3-225.ec2.internal>
2023-04-07 05:14:27 +0000

    Comments and additional cleanup

--- app.js
+++ app.js
@@ -1,55 +1,68 @@
 #!/usr/bin/env node
 
+// import sqlite library installed via npm
 const sqlite3 = require("sqlite3");
+// create a db instance of sqlite backed by the file stufff.db
 const db = new sqlite3.Database("stufff.db")
-// db.run(`CREATE TABLE things (
-//   id INTEGER PRIMARY KEY AUTOINCREMENT,
-//   n INTEGER,
-//   x float(10,9),
-//   y float(10,9),
-//   who VARCHAR(255)
-// )
-// `)
-// db.run(`CREATE TABLE users (
-//   id INTEGER PRIMARY KEY AUTOINCREMENT,
-//   username VARCHAR(255),
-//   pass VARCHAR(255)
-// )
-// `)
-//db.run("INSERT INTO users (username, pass) VALUES ('chuck', 'f52fbd32b2b3b86ff88ef6c490628285f482af15ddcb29541f94bcf526a3f6c7')")
 
-const fs = require("fs/promises");
-const http = require("http"); // import the http library from node
-// "data store" that we'll dump all the things in
-let listOfThings = [];
-// We'll assign unique ids to the incoming data, incrementing
-let lastId = 0;
-
+// import http and the helpers
+const http = require("http");
 const getBody = require("./httpHelpers")
 const { parseBasic, promptForAuth, sha256 } = require("./authHelpers")
 
-/* our authentication mechanism proper. returns username "chuck" if the request contains
- * the Basic authorization header containg the right username and password. Otherwise it
- * prompts for a username and password by sending the appropriate headers and response code
- * to the browser and returns false
+/* our authentication mechanism proper. We're now an async function so we can
+ * await a promise that we'll wrap the sql lookup in. This means we'll need to
+ * await whenever we call this function, which means callers must also be async.
+ * Luckily, our request handler can be async.
+ *
+ * Note, this is actually a huge change in the signature of this method. We're
+ * doing it because it's relatively few key strokes and doesn't change the 
+ * apparent intent much, but deep down, it's huge. iherduleikhardmode.
+ *
+ * Asynchronously returns username if the request contains a Basic authorization 
+ * header containing a username and password found in the `users` table in our 
+ * db. Otherwise it prompts for a username and password by sending the 
+ * appropriate headers and response code to the browser and returns false
  */
 const authenticate = async (req, res) => {
   const [user, pass] = parseBasic(req)
+  // struggle bus here: if we pass an empty string to our `sha256` function, it
+  // explodes. Previously we short circuited on an empty username. Now we check
   if(user && pass) {
+    // oh yeah, you bet auth code is an attack target. let's not trust weirdos
     const lookup = db.prepare(
       "SELECT username FROM users WHERE username=? AND pass=?")
 
-    const soundslike = await new Promise((resolve, reject) => {
+    // We're going to await a promise that we're wrapping around the sqlite call
+    // I'm mildly sorry about the convoluted nonsense here. We probably should
+    // pick one paradigm and run with it. But, I really didn't want to rewrite
+    // all the calls to authenticate to conform to the callback paradigm, and
+    // rewriting to just Promises seemed just as filthy. If all we have to do
+    // to the callers is make them `await`, that might be simpler.
+    //
+    // The point here is that I'm minimizing the impact to the users of this
+    // method by encapsulating this nonsense.
+    //
+    // eventually I settled on this name. ¯\_(ツ)_/¯
+    const userFromDb = await new Promise((resolve, reject) => {
+      // make call to async function that takes a callback
       lookup.get(user, sha256(pass), (err, user) => {
         if(err) {
+          // so, `authenticate` should probably wrap this in a try ... catch
+          // in case there really is an error. but, right now, we wouldn't
+          // have a clue what to do with an error here. server death + stack
+          // trace is "perfect."
           reject(err)
         } else {
+          // no error? resolve with what we got (undefined is what we get if
+          // there's nothing in the DB to match, which is falsy. huray.
           resolve(user && user.username)
         }
       })
     })
-    if(soundslike) {
-      return soundslike
+    // if we got something that wasn't falsy, cool, ship it!
+    if(userFromDb) {
+      return userFromDb
     }
   }
   promptForAuth(res)
@@ -62,16 +75,17 @@ const authenticate = async (req, res) => {
  */
 const requestHandler = async (req, res) => {
   if(req.method === "POST") {
-    // check for auth
+    // check for auth, note await. authenticate is async now, bro.
     const user = await authenticate(req,res)
     // if authed, do the thing
     if(user) {
       getBody(req).then(body => {
+        // prepared statement! "dear database, here's a template. also here's
+        // data to use in that template. Don't mix them up. Sincerely, JS"
         const insert = db.prepare(
           "INSERT INTO things (n, x, y, who) VALUES (?, ?, ?, ?)")
-
         insert.run(body.n, body.x, body.y, user)
-
+        // assuming success, lawl.
         res.writeHead(201)
       })
       .catch(() => res.writeHead(400))
@@ -81,6 +95,8 @@ const requestHandler = async (req, res) => {
     const user = await authenticate(req,res)
     if(user) {
       getBody(req).then(body => {
+        // prepared statement! "dear database, here's a template. also here's
+        // data to use in that template. Don't mix them up. Sincerely, JS"
         const update = db.prepare(
           "UPDATE things SET n=?, x=?, y=?, who=? WHERE id=?")
 
@@ -96,6 +112,8 @@ const requestHandler = async (req, res) => {
     if(user) {
       const match = req.url.match(/\/api\/(\d+)/)
       if(match && match[1]) {
+        // prepared statement! "dear database, here's a template. also here's
+        // data to use in that template. Don't mix them up. Sincerely, JS"
         const remove = db.prepare(
           "DELETE FROM things WHERE id=?")
 
@@ -109,6 +127,8 @@ const requestHandler = async (req, res) => {
       }
     }
   } else {
+    // we rewrite the get to do the async stufff with the database here.
+    // We could have done this kind of thing for authentication too.
     db.all("SELECT * FROM things", (err, things) => {
       if(err) {
         res.writeHead(500)
