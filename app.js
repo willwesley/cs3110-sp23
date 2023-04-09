b60c30cf82470f5ec286d7fa23c023c61d904462
Debian <admin@ip-172-31-3-225.ec2.internal>
2023-04-09 18:07:44 +0000

    Removed fs stuff, add in-mem auth stuff

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 #!/usr/bin/env node
 
-const fs = require("fs/promises");
-const http = require("http"); // import the http library from node
+const http = require("http");
+
 // "data store" that we'll dump all the things in
 let listOfThings = [];
-// We'll assign unique ids to the incoming data, incrementing
 let lastId = 0;
+
+// data store for users with a default admin account
+const users = {
+  chuck: {
+    password: "f52fbd32b2b3b86ff88ef6c490628285f482af15ddcb29541f94bcf526a3f6c7",
+    admin: true
+  }
+};
 
 const getBody = require("./httpHelpers")
 const { parseBasic, promptForAuth, sha256 } = require("./authHelpers")
 
-/* our authentication mechanism proper. returns username "chuck" if the request contains
- * the Basic authorization header containg the right username and password. Otherwise it
+/* our authentication mechanism proper. returns username if the request contains the Basic
+ * authorization header containg a username and password found in users. Otherwise it
  * prompts for a username and password by sending the appropriate headers and response code
  * to the browser and returns false
  */
 const authenticate = (req, res) => {
   const [user, pass] = parseBasic(req)
-  // You will probably want to change this next line to a call to a function that does some
-  // sort of look up and password comparison
-  if(user === "chuck" && sha256(pass) === "f52fbd32b2b3b86ff88ef6c490628285f482af15ddcb29541f94bcf526a3f6c7") {
+  if(users[user] && sha256(pass) === users[user].password) {
     return user
   }
   promptForAuth(res)
@@ -31,7 +36,63 @@ const authenticate = (req, res) => {
  * @param res {http.response} the object for sending a response
  */
 const requestHandler = (req, res) => {
-  if(req.method === "POST") {
+  // handle amdinistration things
+  if(req.url.indexOf("users") !== -1) {
+    const user = authenticate(req,res)
+    // checking for authentication and authorization
+    if(user && users[user].admin) {
+      // probably *shouldn't* combine these, but upsert seems nice
+      if(req.method === "POST" || req.method === "PUT") {
+        getBody(req).then(body => {
+          // if no password provided, use existing one
+          // (this is why probably shouldn't combine)
+          const password = body.pass ?
+              sha256(body.pass) : users[body.user].password
+          // save the user
+          users[body.user] = {
+            password,
+            admin: !!body.admin
+          }
+          // redirect to the admin page, since we're not ajaxing this
+          res.writeHead(301, {
+            Location: "/admin.html"
+          })
+        })
+        .catch(() => res.writeHead(400))
+        .finally(() => res.end())
+      } else if(req.method === "DELETE") {
+        // remove user is a whole lot like remove from listOfThings
+        // we match on /api/users/<any string>
+        const match = req.url.match(/\/api\/users\/(.+)/)
+        if(match && match[1]) {
+          // delete from object is a nicer syntax, I suppose
+          delete users[match[1]]
+          res.writeHead(200)
+          res.end()
+        } else {
+          res.writeHead(400)
+          res.end()
+        }
+      } else {
+        res.writeHead(200)
+        // we really shouldn't send passwords to the front
+        // even if they're hashed.
+        const usersNoP = Object.fromEntries(
+          Object.entries(users)
+            .map(u => [u[0], { admin: u[1].admin }])
+        );
+        res.write(JSON.stringify(usersNoP))
+        res.end()
+      }
+    } else {
+      // if we're here, either unauthenticated or unauthorized
+      // either way, they belong on the main page, not admin
+      res.writeHead(301, {
+        Location: "/"
+      })
+      res.end()
+    }
+  } else if(req.method === "POST") {
     // check for auth
     const user = authenticate(req,res)
     // if authed, do the thing
@@ -41,8 +102,6 @@ const requestHandler = (req, res) => {
         // record creator
         body.who = user;
         listOfThings.push(body);
-        // update on-disk copy
-        fs.writeFile("things.json", JSON.stringify(listOfThings, null, 2))
         res.writeHead(201)
       })
       .catch(() => res.writeHead(400))
@@ -55,7 +114,6 @@ const requestHandler = (req, res) => {
         const idx = listOfThings.findIndex(t => t.id === body.id)
         body.who = user;
         listOfThings[idx] = body
-        fs.writeFile("things.json", JSON.stringify(listOfThings, null, 2))
         res.writeHead(200)
       })
       .catch(() => res.writeHead(400))
@@ -69,7 +127,6 @@ const requestHandler = (req, res) => {
         listOfThings = listOfThings.filter(
           t => t.id != match[1]
         )
-        fs.writeFile("things.json", JSON.stringify(listOfThings, null, 2))
         res.writeHead(200)
         res.end()
       } else {
@@ -78,7 +135,6 @@ const requestHandler = (req, res) => {
       }
     }
   } else {
-    // default assumes GET
     res.writeHead(200, {
       "Content-Type": "application/json",
     })
@@ -89,18 +145,6 @@ const requestHandler = (req, res) => {
   }
 }
 
-// create the server and provide it the handler
 const server = http.createServer(requestHandler);
+server.listen(3000)
 
-// initialize data structure from disk
-fs.readFile("things.json")
-  // first just read in the array
-  .then(things => listOfThings = JSON.parse(things))
-  // the next two lines finds the biggest id in the array
-  .then(things => things.map(t => t.id))
-  .then(ids => Math.max(...ids))
-  // initialize our id counter to avoid conflicting ids
-  .then(maxid => lastId = maxid + 1)
-  .catch(() => listOfThings = [])
-  // now that we're initialized, instruct server to listen on TCP port 3000
-  .finally(() => server.listen(3000))
